76a33b06d409c486839c5f282ec13e292d719ed2
Wendy Hurtado <wendyhurtado95@gmail.com>
2020-09-13 17:05:13 -0400

    variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://muse:190495@cluster0.2g54b.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
