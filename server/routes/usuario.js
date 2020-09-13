acd875ab49816e6565b30afe83fe7f506f0c5f01
Wendy Hurtado <wendyhurtado95@gmail.com>
2020-09-13 16:48:20 -0400

    RestServer - usuario

---
+++ server/routes/usuario.js
@@ -0,0 +1,107 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+
+const app = express();
+
+app.get('/usuarios', (req, resp) => {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email estado role google')
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({ estado: true }, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cantidad: conteo
+                });
+            });
+
+
+        });
+});
+
+app.get('/usuarios/:id', (req, resp) => {
+
+});
+
+app.post('/usuarios', (req, res) => {
+    let body = req.body;
+
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((err, usuario) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario
+        });
+
+    });
+
+});
+
+app.put('/usuarios/:id', (req, res) => {
+    let id = req.params.id;
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'rol', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuario) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario
+        });
+    });
+});
+
+app.delete('/usuarios/:id', (req, resp) => {
+    let id = req.params.id;
+
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true }, (err, usuario) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario
+        });
+    });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 const bodyParser = require('body-parser');
@@ -9,9 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-    res.send('Hola')
-})
+app.use(require('./routes/usuario'))
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true });
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', 3000);
